2d4bcef530578398ca893f136376acb61d9c3efb
ann <annnyza2536@hotmail.com>
2016-06-22 15:13:32 +0700

    image

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     //Explicit
     var strAnswer:String = ""
     var intIndex:Int = 0
+    var arrayImage = ["ishihara_01.png","ishihara_02.phg","ishihara_03.png","ishihara_04.png","ishihara_05.png","ishihara_06.phg","ishihara_07.png","ishihara_08.png","ishihara_09.png","ishihara_10.png"]
+    
     
     
 
@@ -37,8 +39,15 @@ class ViewController: UIViewController {
         strAnswer = String(answerTextField.text)
         print("strAnswer ==> \(strAnswer)")
         
-        intIndex += 1
-        print("intIndex ==> \(intIndex)")
+        if (intIndex < 9) {
+            intIndex += 1
+        }else{
+            intIndex = 0
+        }
+        print("intIndex ===> \(intIndex)")
+        
+        ishiharaImageView.image = UIImage(named: arrayImage[intIndex])
+        
 
     }  // answerButton
     
